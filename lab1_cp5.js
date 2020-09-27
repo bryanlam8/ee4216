37075d20b81fe5d926a72507a9c0c66046d20270
Bryan <bryanlam8-c@my.cityu.edu.hk>
2020-09-27 21:25:35 +0800

    cp5 added

---
+++ lab1_cp5.js
@@ -0,0 +1,9 @@
+function toggle() {
+  var x = document.getElementById("myModal");
+  if (x.style.display == "none") {
+      x.style.display = "flex";
+  }
+  else {
+      x.style.display = "none";
+  }
+}
